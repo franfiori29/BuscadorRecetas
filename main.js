63913bf14cdfed068f1d93adca7bc2bee9045cfb
franfiori29 <franfiori29@gmail.com>
2020-10-04 22:48:08 -0300

    Agregado el botón para agregar un input

--- main.js
+++ main.js
@@ -10,20 +10,43 @@ const getInputs = () => {
   return arrInputs;
 };
 
+const crearTarjeta = (titulo, linkImagen, arrayIngredientes, linkReceta) => {
+  let nuevaTarjeta = document.createElement("div");
+  nuevaTarjeta.setAttribute("class", "tarjeta");
+
+  let elementoTitulo = document.createElement("p");
+  elementoTitulo.innerHTML = titulo;
+  nuevaTarjeta.appendChild(elementoTitulo);
+
+  let elementoImagen = document.createElement("img");
+  elementoImagen.src = linkImagen;
+  nuevaTarjeta.appendChild(elementoImagen);
+
+  arrayIngredientes.forEach((ing) => {
+    let lineaIngrediente = document.createElement("p");
+    lineaIngrediente.innerHTML = ing;
+    nuevaTarjeta.appendChild(lineaIngrediente);
+  });
+
+  let elementoBoton = document.createElement("a");
+  elementoBoton.setAttribute("href", linkReceta);
+  elementoBoton.innerHTML = "IR A LA RECETA";
+
+  nuevaTarjeta.appendChild(elementoBoton);
+  document.getElementById("container").appendChild(nuevaTarjeta);
+};
+
 const getRecipes = () => {
   let inputs = getInputs();
-  let URL =
-    "https://test-es.edamam.com/search?app_id=468b6262&app_key=07d4eaa4c84fba51c3307830dcfbef89&from=" +
-    total +
-    "&to=" +
-    (total + 100) +
-    "&q=" +
-    document.getElementById("ingPrincipal").value;
+  let URL = `https://test-es.edamam.com/search?app_id=468b6262&app_key=07d4eaa4c84fba51c3307830dcfbef89&from=${total}&to=${
+    total + 100
+  }&q=${document.getElementById("ingPrincipal").value}`;
 
   $.ajax({
     url: URL,
     success: function (result) {
       result.hits.forEach((receta) => {
+        console.log(receta);
         receta.recipe.ingredientLines.forEach((linea) => {
           let cont = 0;
           let lineaHtml = "";
@@ -33,18 +56,12 @@ const getRecipes = () => {
             } else break;
           }
           if (cont == inputs.length) {
-            let divCreado = document.createElement("div");
-            divCreado.setAttribute("class", "tarjeta");
-            let nombreReceta = receta.recipe.label;
-            let nuevoTitulo = document.createElement("p");
-            nuevoTitulo.innerText = nombreReceta;
-            divCreado.appendChild(nuevoTitulo);
-            receta.recipe.ingredientLines.forEach((agregar) => {
-              let nuevaLinea = document.createElement("p");
-              nuevaLinea.innerHTML = agregar;
-              divCreado.appendChild(nuevaLinea);
-            });
-            $("#container").append(divCreado);
+            crearTarjeta(
+              receta.recipe.label,
+              receta.recipe.image,
+              receta.recipe.ingredientLines,
+              receta.recipe.url
+            );
           }
         });
       });
@@ -52,8 +69,20 @@ const getRecipes = () => {
   });
 };
 
+document.getElementById("agregarExtra").addEventListener("click", () => {
+  let nuevoInput = document.createElement("div");
+  nuevoInput.innerHTML =
+    'Ingrediente extra: <input type="text" class="extra" /><br />';
+  document.getElementById("divInputs").appendChild(nuevoInput);
+});
+
 document.getElementById("enviar").addEventListener("click", () => {
   getRecipes();
+
+  document.querySelectorAll("img").forEach((imagen) => {
+    if (imagen.height < 32) {
+      imagen.style.display = "none";
+    }
+  });
   total += 100;
-  document.getElementById("enviar").innerText = "Más";
 });
